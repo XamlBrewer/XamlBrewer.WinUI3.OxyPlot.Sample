bd759417f1a051f30cb6a810d6857f448724a0ad
Diederik Krols <diederik_krols@hotmail.com>
2022-02-15 16:17:50 +0100

    Interactive Model: theming

--- XamlBrewer.WinUI3.OxyPlot.Sample/Views/InteractivePage.xaml.cs
+++ XamlBrewer.WinUI3.OxyPlot.Sample/Views/InteractivePage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using XamlBrewer.WinUI3.Services.Theming;
 
 namespace XamlBrewer.WinUI3.OxyPlot.Sample.Views
 {
@@ -7,6 +10,34 @@ namespace XamlBrewer.WinUI3.OxyPlot.Sample.Views
         public InteractivePage()
         {
             InitializeComponent();
+            Loaded += Page_Loaded;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            ApplyTheme(ActualTheme);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            ActualThemeChanged += Page_ActualThemeChanged;
+            base.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            ActualThemeChanged -= Page_ActualThemeChanged;
+            base.OnNavigatedFrom(e);
+        }
+
+        private void Page_ActualThemeChanged(FrameworkElement sender, object args)
+        {
+            ApplyTheme(sender.ActualTheme);
+        }
+
+        private void ApplyTheme(ElementTheme theme)
+        {
+            ViewModel.Model.ApplyTheme(theme);
         }
     }
 }
